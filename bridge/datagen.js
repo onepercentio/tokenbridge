af13ebef07c21912d8a560fc95c52dbe012c0199
pedro <pedro@thinkanddev.com>
2020-11-16 00:43:50 -0300

    Finished rename, fixed integration test

--- bridge/datagen.js
+++ bridge/datagen.js
@@ -7,7 +7,7 @@ const bridgeBuild = require('./build/contracts/Bridge_v2');
 fs.writeFileSync('../abis/Bridge.json', JSON.stringify(bridgeBuild.abi));
 
 const validatorsBuild = require('./build/contracts/Validators_v2');
-fs.writeFileSync('../abis/Federation.json', JSON.stringify(validatorsBuild.abi));
+fs.writeFileSync('../abis/Validators.json', JSON.stringify(validatorsBuild.abi));
 
 const sideTokenBuild = require('./build/contracts/SideToken_v1');
 fs.writeFileSync('../abis/SideToken.json', JSON.stringify(sideTokenBuild.abi));
