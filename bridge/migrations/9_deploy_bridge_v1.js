b42adb2e085565be664d12d20995f0f7ca81af30
pedro <pedro@thinkanddev.com>
2020-11-24 00:54:33 -0300

    Added max min and dialy limits per token basis

--- bridge/migrations/9_deploy_bridge_v1.js
+++ bridge/migrations/9_deploy_bridge_v1.js
@@ -5,28 +5,27 @@ const MultiSigWallet = artifacts.require("MultiSigWallet");
 const proxyAdminAbi = require('../../abis/ProxyAdmin.json');
 
 
-module.exports = function(deployer, networkName, accounts) {
-    deployer.deploy(Bridge_v1)
-    .then(async (bridge_v1) => {
-        const multiSig = await MultiSigWallet.deployed();
-        const bridgeProxy = await Bridge.deployed();
-        // If coverage deploy directly the new contract
-        if (networkName === 'soliditycoverage') {
-            return bridge_v1;
-        }
-        // As we need the multisig to update we can't use oz to do it
-        // We deploy the contract manually and point the proxy to the new logic
-        let jsonName = networkName;
-        const chainId = await web3.eth.net.getId();
-        if((chainId >= 30 && chainId <=33) || chainId == 5777) {
-            jsonName = `dev-${chainId}`;
-        }
-        const networkConfig = require(`../.openzeppelin/${jsonName}.json`);
+module.exports = async (deployer, networkName, accounts) => {
+    await deployer.deploy(Bridge_v1);
+    const bridge_v1 = await Bridge_v1.deployed();
+    const multiSig = await MultiSigWallet.deployed();
+    const bridgeProxy = await Bridge.deployed();
+    // If coverage deploy directly the new contract
+    if (networkName === 'soliditycoverage') {
+        return bridge_v1;
+    }
+    // As we need the multisig to update we can't use oz to do it
+    // We deploy the contract manually and point the proxy to the new logic
+    let jsonName = networkName;
+    const chainId = await web3.eth.net.getId();
+    if((chainId >= 30 && chainId <=33) || chainId == 5777) {
+        jsonName = `dev-${chainId}`;
+    }
+    const networkConfig = require(`../.openzeppelin/${jsonName}.json`);
 
-        const proxyAdminAddress = networkConfig.proxyAdmin.address;
-        const proxyAdmin = new web3.eth.Contract(proxyAdminAbi, proxyAdminAddress);
+    const proxyAdminAddress = networkConfig.proxyAdmin.address;
+    const proxyAdmin = new web3.eth.Contract(proxyAdminAbi, proxyAdminAddress);
 
-        const data = proxyAdmin.methods.upgrade(bridgeProxy.address, bridge_v1.address).encodeABI();
-        await multiSig.submitTransaction(proxyAdmin.options.address, 0, data, { from: accounts[0] });
-      });
+    const data = proxyAdmin.methods.upgrade(bridgeProxy.address, bridge_v1.address).encodeABI();
+    await multiSig.submitTransaction(proxyAdmin.options.address, 0, data, { from: accounts[0] });
 };
